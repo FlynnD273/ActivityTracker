7efaa043187b3d0262a5b5cb8621e9626c4325ad
Flynn Duniho <flynn@harveyosity.org>
2026-03-24 21:39:09 -0700

    Get the progress bar working in the details screen

--- app/src/main/java/com/flynnd273/activitytracker/ui/screens/ActivityScreen.kt
+++ app/src/main/java/com/flynnd273/activitytracker/ui/screens/ActivityScreen.kt
@@ -1,13 +1,12 @@
 package com.flynnd273.activitytracker.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ProgressIndicatorDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -18,6 +17,7 @@ import com.flynnd273.activitytracker.SharedViewModel
 import com.flynnd273.activitytracker.database.ActivityTask
 import com.flynnd273.activitytracker.utils.toTimestamp
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.filterNotNull
 import java.time.Duration
@@ -38,17 +38,36 @@ fun ActivityScreen(viewModel: SharedViewModel, uid: Int) {
             .collect { viewModel.updateActivity(it) }
     }
 
+    var now by remember { mutableStateOf(LocalDateTime.now()) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            now = LocalDateTime.now()
+            delay(500)
+        }
+    }
+
     mutableActivity?.let { activity ->
         Column(modifier = Modifier.fillMaxSize()) {
             TextField(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp),
+                    .padding(8.dp)
+                    .onFocusChanged() {
+                        if (!it.isFocused) {
+                            if (activity.name.isBlank()) {
+                                mutableActivity =
+                                    viewModel.updateActivity(activity.copy(name = activity.uid.toString()))
+                            }
+                        }
+                    },
                 singleLine = true,
                 textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 4.em, textAlign = TextAlign.Center),
                 value = activity.name,
                 onValueChange = { newName ->
-                    if (newName.isNotBlank()) {
+                    if (newName.isBlank()) {
+                        viewModel.updateActivity(activity.copy(name = activity.uid.toString()))
+                    } else {
                         mutableActivity = viewModel.updateActivity(activity.copy(name = newName))
                     }
                 }
@@ -57,7 +76,23 @@ fun ActivityScreen(viewModel: SharedViewModel, uid: Int) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .aspectRatio(1f)
-                    .padding(12.dp),
+                    .padding(12.dp)
+                    .clickable(onClick = {
+                        if (activity.lastStart == null) {
+                            mutableActivity = viewModel.updateActivity(activity.copy(lastStart = now))
+                        } else {
+                            mutableActivity = viewModel.updateActivity(
+                                activity.copy(
+                                    lastStart = null,
+                                    progress = activity.progress +
+                                            (Duration.between(
+                                                activity.lastStart,
+                                                LocalDateTime.now()
+                                            ).seconds)
+                                )
+                            )
+                        }
+                    }),
                 contentAlignment = Alignment.Center
             ) {
                 val density = LocalDensity.current
@@ -69,9 +104,8 @@ fun ActivityScreen(viewModel: SharedViewModel, uid: Int) {
                     derivedStateOf {
                         val baseProgress = activity.progress
                         if (activity.lastStart != null) {
-                            val elapsed =
-                                (Duration.between(activity.lastStart, LocalDateTime.now()).toMillis() / 1000)
-                            baseProgress + elapsed
+                            val elapsed = Duration.between(activity.lastStart, now)
+                            baseProgress + elapsed.seconds
                         } else {
                             baseProgress
                         }
@@ -82,10 +116,13 @@ fun ActivityScreen(viewModel: SharedViewModel, uid: Int) {
                     modifier = Modifier
                         .fillMaxSize(),
                     color = activity.color ?: ProgressIndicatorDefaults.linearColor,
-                    trackColor = ProgressIndicatorDefaults.linearTrackColor,
-                    strokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
                 )
                 Text(progressValue.value.toTimestamp(), fontSize = fontSize)
+            }
+            Button(onClick = {
+                mutableActivity = viewModel.updateActivity(activity.copy(lastStart = null, progress = 0))
+            }) {
+                Text("Reset Progress")
             }
         }
     }
